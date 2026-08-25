811f60136377e65eaba76755ad76ea5d6b6c2798
djorji5803 <you@example.com>
2026-08-25 17:05:46 +0300

    health

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { ResumeModule } from './resume/resume.module';
 import { configModule, graphQLModule } from './config';
+import { HealthController } from './health.controller';
 // TODO Healthcheck
 @Module({
   imports: [configModule, graphQLModule, PrismaModule, ResumeModule],
+  controllers: [HealthController],
 })
 export class AppModule {}
